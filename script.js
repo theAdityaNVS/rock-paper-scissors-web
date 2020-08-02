d6d1509005bce6f76e7912c3cb1bafc541916a70
aditya-nadamuni <aditya.nadamuni@gmail.com>
2020-08-02 16:31:52 +0530

    added variables using DOM selector

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get Variables from DOM 
+const userScore = 0;
+const computerScore = 0;
+const userScoreDisplay = document.getElementById("user-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const scoreBoard = document.getElementsByClassName("scoreDisplay");
+const resultBoard = document.getElementsByClassName("result");
+const rock = document.getElementsById("rock");
+const paper = document.getElementsById("paper");
+const scissors = document.getElementsById("scissors");
+
+// Add Event Listeners to make them Alive 
