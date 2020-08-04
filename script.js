e276ef40c770f58bd8dfe9bbc5243cabccaa3fcf
aditya-nadamuni <aditya.nadamuni@gmail.com>
2020-08-04 16:53:07 +0530

    added computer choice maker and other functions

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-// Get Variables from DOM 
-const userScore = 0;
-const computerScore = 0;
-const userScoreDisplay = document.getElementById("user-score");
-const computerScoreDisplay = document.getElementById("computer-score");
-const scoreBoard = document.getElementsByClassName("scoreDisplay");
-const resultBoard = document.getElementsByClassName("result");
-const rock = document.getElementsById("rock");
-const paper = document.getElementsById("paper");
-const scissors = document.getElementsById("scissors");
+// Variables
+let userScore = 0;
+let computerScore = 0;
+let userScoreDisplay = document.getElementById("user-score");
+let computerScoreDisplay = document.getElementById("computer-score");
+let scoreBoard = document.getElementsByClassName("scoreDisplay");
+let resultBoard = document.getElementsByClassName("result");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let choices = ["rock", "paper", "scissors"];
 
-// Add Event Listeners to make them Alive 
+
+// Make Computer's Choice
+function computerChoice(){
+    let choiceMaker = Math.floor(Math.random() * 3);
+    return choices[choiceMaker];
+}
+
+// Gives Final Result
+function gameMaster(userChoice) {
+    let player = userChoice;
+    let computer = computerChoice();
+    compareChoices(player, computer);
+}
+
+
+
+
+function main(){
+    // Player Choice Functions 
+    rock.onclick = function(){
+        gameMaster(choices[0]);
+    };
+    paper.onclick = function(){
+        gameMaster(choices[1]);
+    };
+    scissors.onclick = function(){
+        gameMaster(choices[2]);
+    };
+}
+
+main();
